41f7633e2119f7096876577e2de3b3cf1986d9a4
shaj684 <48861103+shaj684@users.noreply.github.com>
2019-11-25 14:26:30 -0500

    adding main.cpp with basic preprocessors

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*******************************
+ * Gary Sung, Shawn Jacobsen   *
+ * RJF 12:40 - SDP Project     *
+ * *****************************/
+
+// Preprocessor directives
+#include <stdlib.h>
+#include <stdio.h>
+/*
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHLCD.h>
+*/
