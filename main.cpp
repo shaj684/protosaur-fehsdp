2a929e4b0091e230a68265282f74b4698e77bc55
shaj684 <48861103+shaj684@users.noreply.github.com>
2019-11-25 15:32:14 -0500

    adding dino class and constructor

--- main.cpp
+++ main.cpp
@@ -6,18 +6,30 @@
 // Preprocessor directives
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 /*
 #include <FEHIO.h>
 #include <FEHUtility.h>
 #include <FEHLCD.h>
 */
 
-int main(void) {
-    class Dino {
+// gravity acceleration constant
+#define gravity 1
+
+class dino {
     private:
         int frame, x, y, velocity;
-    };
+        bool onGround;
+        bool jumping;
+    public:
+        dino(int _frame = 0, int _x = 0, int _y = 0, int _velocity = 0, bool _onGround = true, bool _jumping = true);
+        void jump(int x, int y);
+        void hit();
+};
 
+int main(void) {
+
+    /* Default code from QT Creator project init
     float x,y;
 
     LCD.Clear( FEHLCD::Black );
@@ -32,8 +44,15 @@ int main(void) {
         }
     }
     return 0;
+    */
 }
-int main() {
-	// define variables
 
+// Dino Constructor
+dino::dino(int _frame, int _x, int _y, int _velocity, bool _onGround, bool _jumping){
+    frame       = _frame;
+    x           = _x;
+    y           = _y;
+    velocity    = _velocity;
+    onGround    = _onGround;
+    jumping     = _jumping;
 }
